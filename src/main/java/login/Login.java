e08066e914d70f8655433d694f1727c1e79e712b
MARLIN <sanju.reso@gmail.com>
2024-07-19 12:59:07 +0530

    login page_02

--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -5,5 +5,11 @@ public class Login {
 	public void loginFunction() {
 		System.out.println(" This is login page ");
 	}
+	
+	
+	
+	public void loginFunction_02() {
+		System.out.println("this is login page _ 02");
+	}
 
 }
